acc73b6789af7b30b8472a633c3444d6d2c301aa
Eli Hopkins <eh128257@student.wpsok.org>
2025-08-26 13:46:45 -0500

    added AI playground

---
+++ HW3.py
@@ -0,0 +1,43 @@
+#Name: Eli Hopkins
+#Class: 6th Hour
+#Assignment: HW3
+
+#1. Print Hello World!
+print("Hello World!")
+#2. Take the variables below and change the name of the variables to match the type of
+#variable they are: string, integer, or boolean
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
+
+intVar1 = 4
+stringVar2 = "Box"
+boolVar3 = True
+stringVar4 = "Nineteen"
+stringVar5 = "Rope"
+intVar6 = 19
+stringVar7 = "19"
+intVar8 = 2
+boolVar9 = True
+boolVar10 = False
+stringVar11 = "True"
+stringVar12 = "False"
+intVar13 = 5
+stringVar14 = "The"
+stringVar15 = "Game"
+boolVar16 = False
+intVar17 = 3
+intVar18 = 2
+intVar19 = 1
+stringVar20 = stringVar11
+stringVar21 = stringVar12
+stringVar22 = stringVar4
+stringVar23 = stringVar14
+stringVar24 = stringVar15
+intVar25 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
+total = intVar1+intVar25+intVar6+intVar8+intVar13+intVar17+intVar18+intVar19
+print(total)
