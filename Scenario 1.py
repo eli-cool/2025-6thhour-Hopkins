e197f0091bf46db2fc3bcc9e7d460c8a8f0c08eb
Eli Hopkins <eh128257@student.wpsok.org>
2025-09-24 14:09:45 -0500

    added Scenario 1

---
+++ Scenario 1.py
@@ -0,0 +1,46 @@
+#Name: Eli Hopkins
+#Class: 6th Hour
+#Assignment: Scenario 1
+
+#Scenario 1:
+#You are a programmer for a fledgling game developer. Your team lead has asked you
+#to create a nested dictionary containing five enemy creatures (and their properties)
+#for combat testing. Additionally, the testers are asking for a way to input changes
+#to the enemy's damage values for balancing, as well as having it print those changes
+#to confirm they went through.
+
+chars = {
+    "enemy1": {
+        "name":"pitbull named princess",
+        "DMG":4,
+        "HP":6,
+        "AC":3,
+    },
+    "enemy2": {
+        "name":"pitbull named princess",
+        "DMG":4,
+        "HP":6,
+        "AC":3,
+    },
+    "enemy3": {
+        "name":"Ally",
+        "DMG":17,
+        "HP":2007,
+        "AC":"5",
+    },
+    "enemy4": {
+        "name":"the sun",
+        "DMG":2000,
+        "HP":25,
+        "AC":0,
+    },
+    "enemy5": {
+        "name":"skeleton zombie vampire",
+        "DMG":0,
+        "HP":1,
+        "AC":0,
+        #it's just a dead guy
+    }
+}
+
+#It is up to you to decide what properties are important and the theme of the game.
