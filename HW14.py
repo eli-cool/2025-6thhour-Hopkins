a730ec143d034ab440713810eadce680f4f0a6a6
Eli Hopkins <eh128257@student.wpsok.org>
2025-11-12 14:07:14 -0600

    finished HW14

---
+++ HW14.py
@@ -0,0 +1,57 @@
+#Name: Eli Hopkins
+#Class: 5th Hour
+#Assignment: HW14
+
+import time
+#1. Create a for loop with variable i that counts down from 5 to 1 and then prints "Hello World!"
+#at the end.
+"""x = 5
+for i in range(1,5+1):
+    print(x)
+    x -= 1
+    #time.sleep(1)
+print("hellow world")
+#2. Create a for loop that counts up and prints only even numbers between 1 and 30.
+for i in range(1,30+1):
+    if i % 2 == 0:
+        print(i)
+#3. Create a for loop that prints from 1 to 30 and continues (skips the number) if the number is
+#divisible by 3.
+for i in range(1,30+1):
+    if i % 3 != 0:
+        print(i)
+#4. Create a for loop that prints 5 different animals from a list.
+anims = ["dog","cat","bird","goat","monkey"]
+for anim in anims:
+    print(anim)
+#5. Create a for loop that spells out a word you input backwards.
+#(HINT: Google "How to reverse a string in Python")
+for i in str(input("put a word here: "))[::-1]:
+    print(i)
+#6. Create a list containing 10 integers of your choice and print the list.
+#ints = []
+#for i in range(1,10+1):
+#    ints.append(int(input("enter number: ")))
+#    print(i,"out of 10 made")
+#print(ints)
+ints_new = [10,10,9,6,5,7,3,6,1,2]
+print(ints_new)
+#7. Create two empty variables named evenNumbers and oddNumbers.
+evenNumbers = ()
+oddNumbers = ()
+#8. Make a loop that counts the number of even and odd numbers in the list, and prints the
+#result after the loop.
+for i int in ints_new:
+    if i % 2 == 0:
+        evenNumbers += 1
+    else:
+        oddNumbers += 1"""
+#9. Create a variable that asks the user for an integer and an empty integer variable.
+asked = int(input("put a number here: "))
+empty = 1
+#10. Create a loop with a range from 1 to the number the user input. Use the loop to find the
+#factorial of that number and print the result. A factorial of a number is that number multiplied
+#by every number before it until you reach 1. (For example: 5! is 5 x 4 x 3 x 2 x 1 = 120)
+for i in range(1,asked+1):
+    empty *= i
+print(empty)
